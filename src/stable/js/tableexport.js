6c286e3e604413b7e74528af050c6e8b14e3808d
Taras L <t.lopukh@svitla.com>
2017-03-07 14:50:53 +0200

    Styling changes.

--- src/stable/js/tableexport.js
+++ src/stable/js/tableexport.js
@@ -1,5 +1,5 @@
 /*!
- * TableExport.js v3.3.10 (https://www.travismclarke.com)
+ * TableExport.js v3.3.11 (https://www.travismclarke.com)
  * Copyright 2016 Travis Clarke
  * Licensed under the MIT license
  */
@@ -363,7 +363,7 @@
              * Version.
              * @memberof TableExport.prototype
              */
-            version: "3.3.10",
+            version: "3.3.11",
             /**
              * Default plugin options.
              * @memberof TableExport.prototype
